0f7fb2f5b982d26e74976c832a896326502203e7
DXPower <myaka27too@gmail.com>
2015-12-21 23:06:38 -0500

    Creating Project

---
+++ Assets/scripts/BoardGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardGenerator : MonoBehaviour {
+    public List<Tile> tiles = new List<Tile>();
+
+    public GameObject baseHexagonPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
